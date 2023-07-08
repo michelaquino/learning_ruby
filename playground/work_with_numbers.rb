0f605d0215b0df25e48956fcebf4dc93993c02ee
michel.aquino <michel.aquino@g.globo>
2023-07-08 14:47:54 -0300

    number basics

---
+++ playground/work_with_numbers.rb
@@ -0,0 +1,18 @@
+# puts 10 / 4.0
+# puts 10 / 4.to_f
+
+# puts "5" * 2
+20.times {print "-"}
+20.times {puts rand(100)}
+
+20.times {print "-"}
+puts ""
+puts 10 % 3
+
+def multiply(a, b)
+    return a * b
+end
+
+def multiply_2(a, b)
+    a * b
+end
