944bdef3f61391fdd69403dd25cec41739bc47ff
michel.aquino <michel.aquino@g.globo>
2023-07-08 14:20:34 -0300

    String basics

---
+++ playground/string.rb
@@ -0,0 +1,39 @@
+
+# # String concatenation
+# first_name = "Michel"
+# last_name = "Aquino"
+# full_name = first_name + " " +last_name
+
+# puts full_name
+
+# # String interpolation
+# puts "#{first_name} #{last_name}"
+
+# # Methods
+# puts full_name.class
+# # puts full_name.methods
+# puts 10.to_s
+# puts full_name.length
+# puts full_name.reverse
+# puts full_name.capitalize
+# puts full_name.empty?
+# puts full_name.nil?
+# puts "".empty?
+# puts "".nil?
+# puts nil.nil?
+
+# sentence = "Welcome to the jungle"
+# puts sentence
+# sentence = sentence.sub("the jungle", "hell")
+# puts sentence
+
+# Variable assignment
+
+# assignment by value
+name = "Michel"
+another_name = name
+puts name
+puts another_name
+name = "Michel Aquino"
+puts name
+puts another_name
